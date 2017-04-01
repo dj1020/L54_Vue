a92b1ad446d71eebd7479ae474072d8d41a425cc
Ken Lin <kenlin@kkbox.com>
2017-04-01 23:39:19 +0800

    Add /component route and main.js for coding demo

---
+++ resources/views/component.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Styles -->
+    <style>
+        .complete {
+            color: #999;
+        }
+
+        .incomplete {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+
+<div id="root">
+    <h1>Hi Component!</h1>
+</div>
+
+<script src="https://unpkg.com/vue@2.2.6"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="/js/main.js"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/component', function () {
+    return view('component');
+});
