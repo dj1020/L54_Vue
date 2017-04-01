148394a03c3ac2266c3e333c93ba036bd4aefeac
Ken Lin <kenlin@kkbox.com>
2017-04-02 00:25:36 +0800

    Create <message> component by copy/paste

--- resources/views/bulmaMessage.blade.php
+++ resources/views/bulmaMessage.blade.php
@@ -17,19 +17,10 @@
 <body>
 
 <div id="root" class="container">
-    <article class="message">
-        <div class="message-header">
-            <p>Hello World</p>
-            <button class="delete"></button>
-        </div>
-        <div class="message-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>,
-            tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
-            gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>,
-            in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id
-            porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-        </div>
-    </article>
+
+    <message title="Hello World" description="Here is my message"></message>
+    <message title="Call Me" description="Call me when you see this message"></message>
+
 </div>
 
 <script src="https://unpkg.com/vue@2.2.6"></script>
