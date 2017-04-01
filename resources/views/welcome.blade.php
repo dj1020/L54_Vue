f00f5ef46082d5c1d8670e78e6aacb68138899e0
Ken Lin <kenlin@kkbox.com>
2017-04-01 18:21:34 +0800

    Using jquery to do two-way binding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-        </style>
-    </head>
-    <body>
-        Hello Vue
-        <script src="https://unpkg.com/vue@2.2.6"></script>
-    </body>
+    <!-- Styles -->
+    <style>
+    </style>
+</head>
+<body>
+<input type="text" name="message" title="message"/>
+
+<p>Here is your message: <span id="msg"></span></p>
+
+<script src="https://unpkg.com/vue@2.2.6"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<script>
+    var data = {
+        message: "Hello world"
+    };
+
+    $(function () {
+        var msgInput = $('input[name=message]');
+        var msgOutput = $('#msg');
+
+        msgInput.val(data.message);
+        msgOutput.text(data.message);
+        msgInput.on('change', function (e) {
+            data.message = msgInput.val();
+            msgOutput.text(data.message);
+        });
+    });
+</script>
+</body>
 </html>
